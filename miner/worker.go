2fd31ec24fc7faaace2c10cf015179180838a50f
Sean Cheong <sean.k.cheong@gmail.com>
2021-11-08 14:00:01 -0800

    change

--- miner/worker.go
+++ miner/worker.go
@@ -1166,11 +1166,8 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 	}
 
 	// Fill the block with all available pending transactions.
-	pending, err := w.eth.TxPool().Pending(true)
-	if err != nil {
-		log.Error("Failed to fetch pending transactions", "err", err)
-		return
-	}
+	pending := w.eth.TxPool().Pending(true)
+
 
 	// Short circuit if there is no available pending transactions or bundles.
 	// But if we disable empty precommit already, ignore it. Since
